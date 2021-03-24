4ffc4681b496d90516e1978dec662a47e8d254cb
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-03-24 04:39:17 +0300

    İmplements Category component
    Add categories  to combobox and now working categories whit news.

--- NewsSite/src/app/models/category.ts
+++ NewsSite/src/app/models/category.ts
@@ -1,2 +1,4 @@
 export class Category {
+    id:number; 
+    categoryName:string; 
 }
---
+++ NewsSite/src/app/services/category.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { News } from "../models/news";
+import { Photo } from "../models/photo";
+import { AlertifyService } from "./alertify.service";
+import { Router } from "@angular/router";
+import { Category } from '../models/category';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+ 
+
+export class CategoryService {
+
+constructor(private httpClient: HttpClient) { }
+
+  path = "https://localhost:44322/api/";
+
+  getCategoryies(): Observable<Category[]> {
+    return this.httpClient.get<Category[]>(this.path + "categories");
+  }
+  // getCategoriesById(categoryId):Observable<Category>{
+  //   return this.httpClient.get<Category>(this.path+"news/detail/?id="+categoryId)
+  // }
+
+ 
+ 
+}
