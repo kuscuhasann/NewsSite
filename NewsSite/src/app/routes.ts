8ca1a221b12c40ff02f2a392d64c9decf477129e
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-03-27 14:45:11 +0300

    Revert "Fixed LoginGuard providers problem"
    This reverts commit f7fcb0fb81a71ddbf1136a0170834790cf6ce091.

--- NewsSite/src/app/routes.ts
+++ NewsSite/src/app/routes.ts
@@ -7,11 +7,11 @@ import { LoginComponent } from './login/login.component';
 import { LoginGuard } from './login/login.guard';
 export const appRoutes : Routes = [
   
-  { path: "akis", component: NewsComponent },
-  { path: "newsadd", component: NewsAddComponent,canActivate:[LoginGuard]},
-  { path: "haberler/:newsId/:title", component: NewsDetailComponent },
-  { path: "haber/kategoriler/:categoryId/:categoryName", component:NewsComponent},
+  { path: "news", component: NewsComponent },
+  { path: "newsadd", component: NewsAddComponent,canActivate:[LoginGuard] },
+  { path: "newsDetail/:newsId", component: NewsDetailComponent },
+  { path: "news/category/:categoryId", component:NewsComponent},
   { path: "parathormon", component:LoginComponent},
-  { path: "**", redirectTo: "akis", pathMatch: "full" },
+  { path: "**", redirectTo: "news", pathMatch: "full" },
 
 ];
