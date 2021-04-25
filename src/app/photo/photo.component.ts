aab2b43a91844db29cc96a4e663ff680441f29e8
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-04-25 11:47:32 +0300

    Remastered photo component updated versions.
    Now correctly works all systems and versiyon this production succecfully sending message.

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FileUploader} from 'ng2-file-upload'
+import {AlertifyService} from '../services/alertify.service'
+import {AuthService} from '../services/auth.service'
+import {ActivatedRoute} from '@angular/router'
+import {Photo} from '../models/photo'
 
 @Component({
   selector: 'app-photo',
@@ -7,9 +12,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService:AuthService,
+    private alertifyService:AlertifyService, 
+    private activatedRoute:ActivatedRoute) { }
+
+  photos:Photo[]=[];
+  uploader:FileUploader;
+  hasBaseDropZoneOver =false;
+  baseUrl='https://localhost:44322/api/';
+  currentMain:Photo;
+  currentNews:any;
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(params=>{
+      this.currentNews = params["newsId"]
+    })
+    this.initializeUploader();
+  }
+  get isAuthenticated(){
+    return this.authService.loggedIn();
+ }
+
+  initializeUploader(){
+    this.uploader =new FileUploader({
+      url:this.baseUrl +'news/'+this.currentNews+'/photos',
+      authToken: 'Bearer ' +localStorage.getItem('token'),
+      isHTML5: true,
+      allowedFileType : ['image'],
+      autoUpload:false,
+      removeAfterUpload: true,
+      maxFileSize:10*1024*1024
+    })
+
+    this.uploader.onSuccessItem = (item, response, status, headers)=>{
+      if(response){
+        const res :Photo = JSON.parse(response);
+        const photo ={
+          id:res.id,
+          url:res.url,
+          dateAdded:res.dateAdded,
+          isMain:res.isMain,
+          newsId:res.newsId
+        }
+        this.photos.push(photo)
+      }
+    }
   }
 
 }
