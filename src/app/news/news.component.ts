498a2a647953b79f192688d3ec128b7261c69bca
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-04-25 06:02:50 +0300

    News Component Remastered

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,16 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { News } from "../models/news";
+import { NewsService } from "../services/news.service";
+import { ActivatedRoute } from '@angular/router';
+import { Photo } from "../models/photo";
+
+
+
+
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.css']
+  styleUrls: ['./news.component.css'],
+  providers: [NewsService]
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private newsService: NewsService,
+    private activatedRoute:ActivatedRoute,
+
+    ) { }
+
+  news: News[];
+  photos: Photo[] = []
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(params=>{
+     
+      if(params["categoryId"]){
+        this.getNewsForCategories(params["categoryId"])
+      }else{
+        this.getNewsAll()
+      }
+      
+    });
   }
-  
+  getNewsAll() {
+    this.newsService.getNewsAll().subscribe(data=>{
+      this.news = data
+     
+    })   
+  }
 
+  getNewsForCategories(categoryId:number) {
+    this.newsService.getNewsForCategories(categoryId).subscribe(data=>{
+      this.news = data
+    })   
+  }
+
+ 
 }
